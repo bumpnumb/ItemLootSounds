bde96aa90efc50c2c36db378245c880734a2455d
BumpNumb <fennhagen.daniel@hotmail.com>
2022-09-29 14:33:49 -0700

    global

--- main.lua
+++ main.lua
@@ -1,5 +1,32 @@
-require("config")
+BasePath = "Interface\\Addons\\ItemFilter\\Sounds\\"
 
+-- 0 	Poor 	    Poor 	
+-- 1 	Common 	    Common 
+-- 2 	Uncommon 	Uncommon 	
+-- 3 	Rare 	    Rare 	
+-- 4 	Epic 	    Epic 	
+-- 5 	Legendary 	Legendary 	
+-- 6 	Artifact 	Artifact 	
+-- 7 	Heirloom 	Heirloom 	
+-- 8 	WoWToken 	WoW Token 	
+
+Quality = {
+    "nya.wav",
+    "tuturu.mp3",
+    "Wow.mp3",
+    "nyalong.mp3",
+    "Nani.mp3",
+    nil,
+    nil,
+    nil,
+    nil
+};
+
+Item = {
+    {"nyalong.mp3", {"Small Dream Shard"}},
+    {"Nya! arigato.wav", {"Frostweave Cloth", "Infinite Dust"}},
+    {nil, {"Lesser Cosmic Essence"}}
+};
 local delay = 0
 local DEBOUNCE_INTERVAL = 0.3
 
@@ -18,8 +45,8 @@ function ItemFilter()
                 bestQuality = itemInfo.quality;
             end
             
-            for rule=1, #Item do
-                for itemName=1, #Item[2] do
+            for rule = 1, #Item do
+                for itemName = 1, #Item[2] do
                     if itemInfo.item == Item[rule][2][itemName] then
                         bestRule = rule;
                     end
