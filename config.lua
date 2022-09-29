9a299e417c247c17188418be06bb75c24484c189
BumpNumb <fennhagen.daniel@hotmail.com>
2022-09-29 14:15:51 -0700

    Sound logic

---
+++ config.lua
@@ -0,0 +1,29 @@
+BasePath = "Interface\\Addons\\ItemFilter\\Sounds\\"
+
+-- 0 	Poor 	    Poor 	
+-- 1 	Common 	    Common 
+-- 2 	Uncommon 	Uncommon 	
+-- 3 	Rare 	    Rare 	
+-- 4 	Epic 	    Epic 	
+-- 5 	Legendary 	Legendary 	
+-- 6 	Artifact 	Artifact 	
+-- 7 	Heirloom 	Heirloom 	
+-- 8 	WoWToken 	WoW Token 	
+
+Quality = {
+    "nya.wav",
+    "tuturu.mp3",
+    "Wow.mp3",
+    "nyalong.mp3",
+    "Nani.mp3",
+    nil,
+    nil,
+    nil,
+    nil
+};
+
+Item = {
+    {"nyalong.mp3", {"Small Dream Shard"}},
+    {"Nya! arigato.wav", {"Frostweave Cloth", "Infinite Dust"}},
+    {nil, { "Lesser Cosmic Essence"}},
+};
--- main.lua
+++ main.lua
@@ -1,56 +1,41 @@
--- Time delay
 local delay = 0
 local DEBOUNCE_INTERVAL = 0.3
-local open = io.open
-
-local function read_file(path)
-    local file = open(path, "rb") -- r read mode and b binary mode
-    if not file then return nil end
-    local content = file:read "*a" -- *a or *all reads the whole file
-    file:close()
-    return content
-end
-
-local fileContent = read_file("Interface\\Addons\\ItemFilter\\config.txt");
-message(fileContent);
 
 function ItemFilter()
     if GetTime() - delay >= DEBOUNCE_INTERVAL then
         delay = GetTime()
-
-
         info = GetLootInfo();
 
-        -- local n = GetNumLootItem()
-        -- message(tostring(n))
-
-        local itemText;
-        local frostWeave = false;
+        local bestQuality = 0;
+        local bestRule = -1;
+        local bestSound = "";
         
         for i = 1, #info do
             local itemInfo = info[i];
-
-            if itemInfo.item == "Frostweave Cloth" then
-                frostWeave = true;
+            if itemInfo.quality < bestQuality then
+                bestQuality = itemInfo.quality;
             end
-
-
-            if itemText == nil then
-                itemText = itemInfo.item;
-            else
-                itemText = itemText..itemInfo.item;
+            
+            for rule=1, #Item do
+                for item=1, #Item[2] do
+                    if itemInfo.item == Item[rule][2][item] then
+                        bestRule = rule;
+                    end
+                end
             end
-
         end
 
-        if frostWeave == true then
-            PlaySoundFile("Interface\\Addons\\ItemFilter\\Sounds\\nyalong.mp3");
+
+        if bestRule == -1 then
+            bestSound = Quality[bestQuality];
         else
-            PlaySoundFile("Interface\\Addons\\ItemFilter\\Sounds\\pinnes.mp3");
+            bestSound = Item[bestRule][1]
+        end
+
+        if bestSound ~= "" then
+            PlaySoundFile(BasePath..bestSound);
         end
     end
-
-    -- message(itemText);
 end
 
 
