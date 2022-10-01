25c3d9351306952c6b3ab7daecf9e8b8c4b12e63
BumpNumb <fennhagen.daniel@hotmail.com>
2022-10-01 07:09:56 -0700

    Sound update

--- main.lua
+++ main.lua
@@ -1,22 +1,22 @@
 BasePath = "Interface\\Addons\\ItemLootSounds\\Sounds\\";
 
--- 0 	Poor 	    Poor 	
--- 1 	Common 	    Common 
--- 2 	Uncommon 	Uncommon 	
--- 3 	Rare 	    Rare 	
--- 4 	Epic 	    Epic 	
--- 5 	Legendary 	Legendary 	
--- 6 	Artifact 	Artifact 	
--- 7 	Heirloom 	Heirloom 	
--- 8 	WoWToken 	WoW Token 	
+-- 0 	Poor 	    Poor 	    Gray
+-- 1 	Common 	    Common      White
+-- 2 	Uncommon 	Uncommon 	Green
+-- 3 	Rare 	    Rare 	    Blue
+-- 4 	Epic 	    Epic 	    Purple
+-- 5 	Legendary 	Legendary 	Orange
+-- 6 	Artifact 	Artifact 	Beige
+-- 7 	Heirloom 	Heirloom 	Beige
+-- 8 	WoWToken 	WoW Token 	Turquoise
 
 QualityRules = {
     "",
     "",
-    "nya.wav",
-    "tuturu.mp3",
-    "pinnes.mp3",
-    "Wow.mp3",
+    "green_item.mp3",
+    "blue_item.mp3",
+    "woo_epic.mp3",
+    "legendaryyyy.mp3",
     "nyalong.mp3",
     "nyalong.mp3",
     "nyalong.mp3"
@@ -34,9 +34,19 @@ QualityRules = {
 -- ItemRules always takes priority over QualityRules
 
 ItemRules = {
-    {"nya.wav", {"Small Dream Shard", "Lesser Cosmic Essence"}},
-    {"tuturu.mp3", {"Frostweave Cloth", "Infinite Dust"}},
+    {"", {"Small Dream Shard", "Lesser Cosmic Essence"}},
+    {"", {"Frostweave Cloth", "Infinite Dust"}},
+    {"", {"Copper Ore", "Tin Ore", "Silver Ore", "Iron Ore", "Gold Ore", "Mithril Ore", "Truesilver Ore", "Dark Iron Ore", "Fel Iron Ore", "Adamantite Ore"}},
+    {"cobalt_ore.mp3", {"Cobalt Ore"}},
+    {"saronite_ore.mp3", {"Saronite Ore"}},
+    {"titanium_ore.mp3", {"Titanium Ore"}},
+    {"eternal.mp3", {"Eternal Might", "Eternal Shadow", "Eternal Earth", "Eternal Fire", "Eternal Water", "Eternal Air", "Eternal Life", "Eternal Mana [PH]"}},
 };
+
+
+
+-- ################################# IGNORE REST #################################
+
 
 local delay = 0
 local DEBOUNCE_INTERVAL = 0.3
@@ -76,8 +86,11 @@ end
 
 
 
-local itemFilter = CreateFrame("frame")
-itemFilter:RegisterEvent("Loot_Ready")
-itemFilter:SetScript("OnEvent", ItemFilter)
+local itemFilter = CreateFrame("frame");
+itemFilter:RegisterEvent("Loot_Ready");
+itemFilter:SetScript("OnEvent", ItemFilter);
 
 
+local rollFilter = CreateFrame("frame");
+rollFilter:RegisterEvent("START_LOOT_ROLL");
+rollFilter:SetScript("OnEvent", function(self) PlaySoundFile(BasePath.."time_to_roll.mp3"); end);
